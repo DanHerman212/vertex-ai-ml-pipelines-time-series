e478aac171f7d77420b51d495499f4438308ab54
Dan Herman <dan.herman@me.com>
2025-12-15 19:21:12 -0500

    updated pipeline code to include evaluation step

--- pipeline.py
+++ pipeline.py
@@ -60,6 +60,22 @@ def train_gru_component(
         ]
     )
 
+@dsl.container_component
+def evaluate_model_component(
+    input_csv: dsl.Input[dsl.Dataset],
+    model_dir: dsl.Input[dsl.Model],
+    metrics: dsl.Output[dsl.Metrics]
+):
+    return dsl.ContainerSpec(
+        image='us-east1-docker.pkg.dev/time-series-478616/ml-pipelines/gru-training:v1',
+        command=["python", "src/evaluate_models.py"],
+        args=[
+            "--input_csv", input_csv.path,
+            "--model_dir", model_dir.path,
+            "--metrics_output_path", metrics.path
+        ]
+    )
+
 # 3. Pipeline Definition
 @dsl.pipeline(
     name="gru-training-pipeline",
@@ -93,6 +109,12 @@ def gru_pipeline(
     train_gru_task.set_memory_limit('16G')
     train_gru_task.set_gpu_limit(1)
     train_gru_task.set_accelerator_type('NVIDIA_TESLA_T4')
+
+    # Step 4: Evaluate
+    evaluate_task = evaluate_model_component(
+        input_csv=preprocess_task.outputs["output_csv"],
+        model_dir=train_gru_task.outputs["model_dir"]
+    )
 
 if __name__ == "__main__":
     compiler.Compiler().compile(
