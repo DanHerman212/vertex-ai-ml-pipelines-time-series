f8de3899488e21d3baa81d658af60f1f0b2be75c
Dan Herman <dan.herman@me.com>
2025-12-23 17:46:19 -0500

    updated variables with latest image tag to fix pipeline issues with metadata

--- scripts/image_variables.sh
+++ scripts/image_variables.sh
@@ -2,13 +2,13 @@
 export $(cat .env | grep -v '#' | awk '/=/ {print $1}')
 
 
-# OLD_TAG="v20251221-225104"
+
 REPO=${REPO_NAME:-ml-pipelines} # Default to ml-pipelines if not set
 
-old_pytorch_tag="v20251221-225104"
+old_pytorch_tag="v20251223-213439"
 export PYTORCH_IMAGE_URI="us-east1-docker.pkg.dev/${PROJECT_ID}/${REPO}/pytorch-training:${old_pytorch_tag}"
 
-old_pytorch_serving_tag="v20251221-225104"
+old_pytorch_serving_tag="v20251223-213439"
 export PYTORCH_SERVING_IMAGE_URI="us-east1-docker.pkg.dev/${PROJECT_ID}/${REPO}/pytorch-serving:${old_pytorch_serving_tag}"
 
 old_tf_tag="v20251221-225104"
